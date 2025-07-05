8b63594baf9cbd0a9251fc5e218dd0ab32c98910
rutvijmavani <rutvijmavani@gmail.com>
2025-07-05 06:29:16 -0400

    first commit

---
+++ 01vite_react/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./Chai"
+
+function App() {
+  return (
+    <>
+      <h1>
+        Chai aur React with Vite | Rutvij
+      </h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
+
