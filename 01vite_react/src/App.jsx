22859ce96d64c212486015aa8775f224fb6766c3
rutvijmavani <rutvijmavani@gmail.com>
2025-07-07 11:26:34 -0400

    counter

--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -1,11 +1,13 @@
 import Chai from "./Chai"
 
 function App() {
+  const username = "chai aur code"
   return (
     <>
       <h1>
         Chai aur React with Vite | Rutvij
       </h1>
+      <p> {username} </p>
       <Chai />
     </>
   )
